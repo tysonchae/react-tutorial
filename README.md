32c7212a3c9d05cd50c957ab079af763f46886dd
tysonchae <tysonchae@gmail.com>
2018-03-05 20:36:18 -0800

    Iteration - map usage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-tutorial
 react 연습
+Day 1. Component 생성, State와 Props 사용 (https://velopert.com/921)
+Day 2. Component Iteration - Map
--- src/components/App.js
+++ src/components/App.js
@@ -6,41 +6,50 @@ import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
 
-    constructor(props){
-        super(props);
-
-        this.state={
-            value : Math.round(Math.random()*100)
-        };
-
-        this.updateValue = this.updateValue.bind(this);
-    }
-
-    updateValue(randomValue){
-        this.setState({
-            value: randomValue
-        
-        });
-    }
-
     render(){
 
         return (
+            <Contacts/>
+        );
+    }
+}
+
+class Contacts extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            contactData: [
+                {name: "Abet", phone: "010-0000-0001"},
+                {name: "Betty", phone: "010-0000-0002"},
+                {name: "Charlie", phone: "010-0000-0003"},
+                {name: "David", phone: "010-0000-0004"}
+            ]
+        };
+    }
+    render(){
+        return(
             <div>
-                <Header title={this.props.headerTitle}/>
-                <Content title={this.props.contentTitle} 
-                         body={this.props.contentBody}/>
-                         <RandomNumber number={this.state.value}
-                         onUpdate={this.updateValue}/>
+                <h1>Contacts</h1>
+                <ul>
+                    {this.state.contactData.map((contact, i) => {
+                        return (<ContactInfo name={contact.name}
+                                            phone={contact.phone}
+                                              key={i} 
+                                 />);
+                    })}
+                </ul>
             </div>
         );
     }
 }
 
-App.defaultProps = {
-    headerTitle: 'Default header',
-    contentTitle: 'Default contentTitle',
-    contentBody: 'Default contentBody'
-};
+class ContactInfo extends React.Component {
+    render(){
+        return(
+            <li>{this.props.name}{this.props.phone}</li>
+        );
+    }
+}
 
 export default App;
